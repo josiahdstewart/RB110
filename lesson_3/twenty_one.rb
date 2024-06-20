9d93af6bd9e1c95f516c53c407bec461597c4b7e
Josiah Stewart <josiahdstewart@gmail.com>
2024-06-20 03:16:16 -0500

    added a basic display hands method

--- lesson_3/twenty_one.rb
+++ lesson_3/twenty_one.rb
@@ -1,6 +1,7 @@
 require 'byebug'
-CARDS = ['1','2','3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+RANK = ['1','2','3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 SUITS = ['H', 'D', 'C', 'S']
+SUITS_SYMBOLS = {'H' => "♥",'D' => "♦", 'C' => "♣", 'S' => "♠"}
 OPENING_CARDS = 2
 HIT_CARD = 1
 
@@ -11,7 +12,7 @@ end
 def initialize_deck
   new_deck = []
   SUITS.each do |suit|
-    CARDS.each { |card| new_deck << [suit, card] }
+    RANK.each { |rank| new_deck << [rank, suit] }
   end
   new_deck
 end
@@ -19,7 +20,7 @@ end
 def deal_cards!(deck)
   hand = []
   OPENING_CARDS.times do |num|
-    hand << hit(deck)
+    hand << hit!(deck)
   end
   hand
 end
@@ -30,10 +31,21 @@ def hit!(deck)
   card
 end
 
+def display_hands(dealer_hand, player_hand)
+  system 'clear'
+  puts "DEALER : #{dealer_hand[0][0] + SUITS_SYMBOLS[dealer_hand[0][1]]} | UNKNOWN"
+  puts "YOU    : #{player_hand[0][0] + SUITS_SYMBOLS[player_hand[0][1]]} | #{player_hand[1][0] + SUITS_SYMBOLS[player_hand[1][1]]}" 
+end
+
 loop do
-  byebug
+  #byebug
   deck = initialize_deck
   player_hand = deal_cards!(deck)
   dealer_hand = deal_cards!(deck)
+  display_hands(dealer_hand, player_hand)
+  loop do
+    prompt "Hit or stay?"
+    break
+  end
   break
 end
