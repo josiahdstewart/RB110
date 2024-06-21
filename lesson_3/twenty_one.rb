e81eebaac3f6902eb27515053f66c0d34a88a59c
Josiah Stewart <josiahdstewart@gmail.com>
2024-06-21 14:48:16 -0500

    added ability to count to 5 rounds

--- lesson_3/twenty_one.rb
+++ lesson_3/twenty_one.rb
@@ -1,5 +1,5 @@
 require 'byebug'
-RANK = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+RANK = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 SUITS = ['H', 'D', 'C', 'S']
 SUITS_SYMBOLS = { 'H' => "♥", 'D' => "♦", 'C' => "♣", 'S' => "♠" }
 FACE_CARD_VALUE = 10
@@ -56,8 +56,14 @@ def display_player_hand(hand)
   puts player_hand
 end
 
-def display_hands(dealer_hand, player_hand)
+def display_hands(dealer_hand, player_hand, dealer_score, player_score)
   system 'clear'
+  puts "First to 5 wins the championship!"
+  puts "        SCORE"
+  puts "DEALER | #{dealer_score}"
+  puts "YOU    | #{player_score}"
+  puts ''
+  puts "        HANDS"
   display_dealer_hand(dealer_hand)
   display_player_hand(player_hand)
 end
@@ -96,65 +102,91 @@ def bust?(total_value)
   total_value > TARGET
 end
 
-def display_results(dealer_hand, player_hand)
-  if total(dealer_hand) > total(player_hand)
-    prompt "DEALER WON: #{total(dealer_hand)}-#{total(player_hand)}"
-  elsif total(dealer_hand) < total(player_hand)
-    prompt "YOU WON: #{total(player_hand)}-#{total(dealer_hand)}"
+def who_won(dealer_total, player_total)
+  if dealer_total > player_total
+    "DEALER WON"
+  elsif dealer_total < player_total
+    "YOU WON"
   else
-    prompt "YOU TIED`: #{total(dealer_hand)}-#{total(player_hand)}"
+    "TIE MATCH"
   end
 end
 
-def player_turn(deck, dealer_hand, player_hand)
+def display_results(dealer_total, player_total)
+  prompt who_won(dealer_total, player_total) + ": #{dealer_total}-#{player_total}"
+end
+
+def player_turn(deck, dealer_hand, player_hand, dealer_score, player_score)
   loop do
     prompt "Hit or stay? (h/s)"
     answer = gets.chomp
     player_hand << hit!(deck) if answer.start_with?('h')
-    display_hands(dealer_hand, player_hand)
+    display_hands(dealer_hand, player_hand, dealer_score, player_score)
     break if bust?(total(player_hand)) || answer.start_with?('s')
   end
 end
 
-def dealer_turn(deck, dealer_hand, player_hand)
+def dealer_turn(deck, dealer_hand, player_hand, dealer_score, player_score)
   loop do
     break if total(dealer_hand) >= DEALER_HIT_COUNT
     dealer_hand.prepend(hit!(deck))
-    display_hands(dealer_hand, player_hand)
+    display_hands(dealer_hand, player_hand, dealer_score, player_score)
   end
 end
 
 def play_again?
   prompt "Do you want to play again? (y/n)"
-  gets.chomp.downcase.start_with?('y')
+  !gets.chomp.downcase.start_with?('n')
 end
 
+def keep_playing?
+  prompt "Keep playing? (y/n)"
+  !gets.chomp.downcase.start_with?('n')
+end
+
+player_score = 0
+dealer_score = 0
+
 loop do
-  byebug
   deck = initialize_deck
   player_hand = deal_cards!(deck)
   dealer_hand = deal_cards!(deck)
-  display_hands(dealer_hand, player_hand)
+  display_hands(dealer_hand, player_hand, dealer_score, player_score)
 
-  player_turn(deck, dealer_hand, player_hand)
+  player_turn(deck, dealer_hand, player_hand, dealer_score, player_score)
   player_total = total(player_hand)
 
   if bust?(player_total)
     prompt "DEALER WON. YOU BUSTED WITH #{(player_total)}"
+    dealer_score += 1
+    break unless keep_playing?
   else
     prompt "You stayed!"
-    dealer_turn(deck, dealer_hand, player_hand)
+    dealer_turn(deck, dealer_hand, player_hand, dealer_score, player_score)
   end
 
   dealer_total = total(dealer_hand)
 
   if bust?(dealer_total)
     prompt "YOU WON. THE DEALER BUSTED WITH #{dealer_total}"
+    player_score += 1
+    break unless keep_playing?
   elsif !bust?(player_total)
-    display_results(dealer_hand, player_hand)
+    display_results(dealer_total, player_total)
+    if who_won(dealer_total, player_total).start_with?('D')
+      dealer_score += 1
+    elsif who_won(dealer_total, player_total).start_with?('Y')
+      player_score += 1
+    end
+    break unless keep_playing?
   end
 
-  break unless play_again?
+  if player_score == 5 || dealer_score == 5
+    prompt "#{who_won(dealer_score, player_score)} THE CHAMPIONSHIP!"
+    break unless play_again?
+    player_score = 0
+    dealer_score = 0
+  end
 end
 
 prompt "Thanks for playing Twenty-One!"
